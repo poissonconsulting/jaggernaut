2af851b7482ba1fc93b2793b63abd252a16cba61
Joe Thorley <joe@poissonconsulting.ca>
2013-02-06 15:49:15 -0800

    fixed bug in get data

--- R/get-data.r
+++ R/get-data.r
@@ -1,7 +1,14 @@
 
+#' Get data from JAGS analysis
+#'
+#' Returns data from a JAGS analysis
+#' 
+#' @param object a JAGS analysis
+#' @param data.frame of the data used in the JAGS analysis
+#' @export
 get_data <- function (object) {
   if(!is.janalysis (object)) 
     stop ("object must be of class janalysis")
 
-  return (object$data)  
+  return (object$analyses[[1]]$data)  
 }
