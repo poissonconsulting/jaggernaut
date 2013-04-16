600337374938ba20fdc47d03a00add30b379618d
Joe Thorley <joe@poissonconsulting.ca>
2013-04-16 10:33:31 -0700

    added help files for peregrine data set and updated help files for jmodel

---
+++ R/data.r
@@ -0,0 +1,21 @@
+#' Peregrine falcon breeding population
+#'
+#'
+#' The peregrine falcon (\emph{Falco peregrinus}) population
+#' breeding in the French Jura from 1964 to 2003 (data courtesy of R.-J. Monnerett).
+#' The variables are as follows:
+#' \itemize{
+#'   \item \code{Pairs} an integer vector of the number of adult pairs.
+#'   \item \code{R.pairs} an integer vector of the number of reproductive pairs.
+#'   \item \code{Eyasses} an integer vector of the number of fledged young.
+#' }
+#' The data are analysed by Kery & Schaub (2012).
+#'
+#' @docType data
+#' @name peregrine
+#' @usage peregrine
+#' @format A data frame with 40 rows and 3 variables
+#' @references Kery & Schaub (2012) Bayesian Population Analysis
+#' using WinBUGS. Academic Press, Oxford.
+#' @keywords datasets
+NULL
