f42b8b38373c9c40609d4eaed9d2f153e0562bbf
Joe Thorley <joe@poissonconsulting.ca>
2013-09-03 16:04:57 -0700

    updated jags_simulation os multiple reps and expects jags_data_model

--- R/jags-simulation.r
+++ R/jags-simulation.r
@@ -3,19 +3,19 @@
 #'
 #' @description 
 #' Performs a JAGS simulation by using a 
-#' \code{jags_model} and a data frame of values  
+#' \code{jags_data_model} and a data frame of values  
 #' to generate a simulation data frame using JAGS (Plummer 2012). 
 #' 
-#' @param model a \code{jags_model}.
+#' @param model a \code{jags_data)model}.
 #' @param values a data.frame of values.
 #' @param quiet a logical element indicating whether to print updates.
 #' @return a \code{jags_simulation} object
 #' @references 
 #' Plummer M (2012) JAGS Version 3.3.0 User Manual \url{http://sourceforge.net/projects/mcmc-jags/files/Manuals/}
-#' @seealso \code{\link{jags_model}} and \code{\link{jaggernaut}}
+#' @seealso \code{\link{jags_data_model}} and \code{\link{jaggernaut}}
 #' @examples
 #' 
-#' mod <- jags_model("
+#' mod <- jags_data_model("
 #' data { 
 #'  for (i in 1:nx) { 
 #'    x[i] ~ dpois(bIntercept) 
@@ -29,16 +29,28 @@
 #'
 #' val <- data.frame(nx = 10, bIntercept = 10)
 #' 
-#' sim <- jags_simulation (mod, val, quiet = TRUE)
+#' sim <- jags_simulation (mod, val, nrep = 100, quiet = FALSE)
 #' 
 #' @export
-jags_simulation <- function (model, values, quiet = opts_jagr("quiet")) {
+jags_simulation <- function (data_model, values, nrep = 1, quiet = opts_jagr("quiet")) {
   
-  if (!is.jags_model(model)) 
+  model <- data_model
+  
+  if (!is.jags_data_model(model)) 
     stop("model must be class jags_model")
   
   if(!(is.data.frame(values) && nrow(values) == 1))
     stop ("values must be a data frame with one row of parameter values")
+
+  nrep <- as.integer(nrep)
+  if(!is.numeric(nrep))
+    stop("quiet must be class integer")
+  
+  if(!length(nrep) == 1)
+    stop("nrep must be a single value")
+  
+  if(nrep < 1)
+    stop("nrep must be positive")
   
   if(!is.logical(quiet))
     stop("quiet must be class logical")
@@ -55,13 +67,19 @@ jags_simulation <- function (model, values, quiet = opts_jagr("quiet")) {
   if(!"bugs" %in% list.modules())
     load.module("bugs")
   
-  object <- jagr_simulation(model = model, data = values, quiet = quiet)
+  data <- list()
+  for (i in 1:nrep) {
+    if (!quiet)
+      print(paste("Rep:",i))
     
-  est <- calc_estimates(object)
-   
-  est <- est[rownames(est) != "deviance",]
+    object <- jagr_simulation(model = model, data = values, quiet = quiet)
+
+    est <- calc_estimates(object)
     
-  data <- extract_estimates(est)[["estimate"]]
+    est <- est[rownames(est) != "deviance",]
+    
+    data[[i]] <- extract_estimates(est)[["estimate"]]
+  }
   
   object$simulated <- data
   
@@ -69,3 +87,48 @@ jags_simulation <- function (model, values, quiet = opts_jagr("quiet")) {
   
   return (object)
 }
+
+#' ## jags_simulation()
+#' 
+#' ## so need option to have multiple rows in values
+#' ## need to take quiet from mode
+#' ## note debug mode different to quiet i.e. if in debug mode proper
+#' ## then show model fitting information plus in debug mode
+#' ## nrep = 1 maybe set nrep in mode?
+#' ## once fitted need ability to pull out data lists by values and rep 
+#' ## and also variable names some function like 
+#' 
+#' ## data_set(sim, value = NULL, rep = NULL, variables = NULL)
+#' 
+#' ## note I think jags_data_models no conversion in select i.e. (*+
+#' ## also only allow integer and numeric
+#' ## enhancement possibility to specify type and range for things 
+#' ## in select
+#' ## could be used to automatically generate values
+#' ## get this sorted so useful for other programs in and of itself
+#' 
+#' ## then write 
+#' 
+#' ## jags_power (...,niter = 10^3) 
+#' 
+#' #' ## note if not converged then keeps but records that not conv
+#' # option to update number of iter if use then update those that not 
+#' # converged..
+#' ## applies jags_model to each simulated data and gets parameter
+#' ## key requirement to add reps so that do in stages
+#' 
+#' ## update(power, nrep = 1, values = NULL, niter = NULL)
+#' 
+#' ## finally ability to add power analyses 
+#' 
+#' ## add(power1, power2)
+#' 
+#' ## must have same jags_data_model and if not same number of reps
+#' ## will update shorter one finally if shared values will drop
+#' ## value from power with less reps or second power if same number
+#' ## of reps also if different. also update niter so same (only update
+#' unconverged iterations...)
+#' 
+#' # finally need ability to query out power different parameters
+
+#' power(power1)
