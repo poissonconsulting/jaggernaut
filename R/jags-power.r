5d98b6bc6d6dee0db73c016deaf1fd088f0df088
Joe Thorley <joe@poissonconsulting.ca>
2013-09-08 14:56:24 -0700

    first attempt at jags_power

--- R/jags-power.r
+++ R/jags-power.r
@@ -1,73 +1,129 @@
 
-jags_power <- function (model, data_block, values, nreps = 100, pars = NULL, debug = FALSE, ...
-)
-{
-  #   if(!is.gsmodel(model_block))
-  #     stop ("model_block should be class gsmodel")
-  # 
-  #   if(!is.gsdata(data_block))
-  #     stop ("data_block should be class gsdata")
-  #   
-  #   if(!(is.data.frame(values) && nrow(values) >= 1))
-  #     stop ("values should be a data frame where each row is the parameter values")
-  # 
-  #   if(!(length(nreps) == 1 && nreps[1] >= 1))
-  #     stop("nreps should be a single integer >= 1")
-  #   
-  #   if (!(is.null(pars) || (is.numeric(pars) && !is.null(pars))))
-  #     stop ("pars must be NULL or a named numeric vector")
-  #   
-  #   if(!(is.logical(debug) && length(debug) == 1 && !is.null(debug)))
-  #     stop("debug must be single logical value")
-  #   
-  #   if(debug)
-  #     nreps <- 1
-  #   
-  #   nreps <- as.integer(nreps)
-  #     
-  #   object<-list(
-  #     model_block = model_block,
-  #     data_block= data_block,
-  #     values = values,
-  #     nvalues = nrow(values),
-  #     nreps = nreps,
-  #     nconfail = NULL, 
-  #     analyses = NULL,
-  #     parnames = NULL,
-  #     parvalues = NULL,
-  #     power = NULL
-  #   )
-  #   analyses = list()
-  #   
-  #   nconfail <- rep(0, object$nvalues)
-  #   
-  #   for (i in 1:object$nvalues) {
-  #     rep <- list()
-  #     for (j in 1:object$nreps) {
-  #       repeat {
-  #         print (paste("values",i,"of",object$nvalues))
-  #         print (paste("rep",j,"of",object$nreps))
-  # 
-  #         sim<- gssimulation (block = data_block, as.data.frame(values[i,,drop=F]), debug = debug)
-  #         analysis <- gsanalysis (block = model_block, data = sim$data, debug = debug, ...)
-  #   
-  #         rep[[j]] <- analysis
-  #         
-  #         if (check_convergence(analysis))
-  #           break
-  #         nconfail[i] <-  nconfail[i] + 1
-  #         if(debug)
-  #           break
-  #       }
-  #     }
-  #     analyses[[i]] <- rep
-  #   }
-  #   object$nconfail <- nconfail
-  #   object$analyses <- analyses
-  #   
-  #   class(object) <- c("gspower")
+
+
+#' @title Perform a JAGS simulation
+#'
+#' @description 
+#' Performs a JAGS simulation by using a 
+#' \code{jags_data_model} and a data frame of values  
+#' to generate a simulation data frame using JAGS (Plummer 2012). 
+#' 
+#' @param data_model a \code{jags_data_model}.
+#' @param nrep an integer element indicating the number of datasets to generate for each set of input values.
+#' @param values a data.frame of input values.
+#' @param mode a character element indicating the mode for the analysis.
+#' @return a \code{jags_simulation} object
+#' @references 
+#' Plummer M (2012) JAGS Version 3.3.0 User Manual \url{http://sourceforge.net/projects/mcmc-jags/files/Manuals/}
+#' @seealso \code{\link{jags_data_model}} and \code{\link{jaggernaut}}
+#' @examples
+#' 
+#' data_model <- jags_data_model("
+#' data { 
+#'  for (i in 1:ny) { 
+#'    y[i] ~ dpois(bIntercept) 
+#'  } 
+#'}    
+#' ")
+#' 
+#' model <- jags_data_model("
+#' model { 
+#' bIntercept ~ dunif(0, 100)
+#'  for (i in 1:ny) { 
+#'    y[i] ~ dpois(bIntercept) 
+#'  } 
+#'}    
+#' ")
+#'
+#' values <- data.frame(ny = c(10, 100), bIntercept = c(10,10))
+#' 
+#' power <- jags_power (model, data_model, values = values, nrep = 100, mode = "demo")
+#' @export
+jags_power <- function (model, data_model, values, nrep = 100, niter = 10^3, mode = "current") {
+  if (!is.jags_model(model)) 
+    stop("model must be class jags_model")
+  
+  if (!is.jags_data_model(data_model)) 
+    stop("data_model must be class jags_data_model")
+  
+  if(!is.data.frame(values))
+    stop ("values must be a data frame")
+  
+  if(!is.data.frame(values))
+    stop ("values must be a data frame")
+  
+  if(nrow(values) == 0)
+    stop ("values must have at least one row of data")
+  
+  if(ncol(values) == 0)
+    stop ("values must have at least one column of data")
+  
+  if(!is.numeric(nrep))
+    stop("nrep must be class integer")
+  
+  if(!length(nrep) == 1)
+    stop("nrep must be a single value")
+  
+  if(nrep < 1)
+    stop("nrep must be positive")
+  
+  nrep <- as.integer(nrep)
+  
+  if(!"basemod" %in% list.modules())
+    load.module("basemod")  
+  
+  if(!"bugs" %in% list.modules())
+    load.module("bugs")
+  
+  old_opts <- opts_jagr(mode = mode)
+  on.exit(opts_jagr(old_opts))
+  
+  if(opts_jagr("mode") == "debug") {
+    nrep <- 1
+  }
+  nvalues <- nrow(values)
+  
+  sim <- jags_simulation(data_model = data_model, 
+                         values = values, 
+                         nrep = 1, mode = "explore")
+  
+  analyses <- list()
+  convergence <- matrix(nvalues, 1)
+  
+  for (value in 1:nvalues) {
+    analyses[[value]] <- list()
+    rep <- 1
+    if (!opts_jagr("quiet"))
+      print(paste0("Value: ",value," of ",nvalues,"  Rep: ", rep," of ",nrep))
+    
+    an <- jags_analysis(model = model, 
+                        data = dataset(sim, rep = rep, value = value),
+                        niter = niter)
+    
+    analyses[[value]][[rep]] <- an
+    
+    convergence[value,rep] <- convergence(an)$convergence
+  }
+  
+  object <- list(
+    model = model,
+    data_model = data_model, 
+    values = values,
+    nvalues = nrow(values),
+    nrep = 1,
+    simulated = sim,
+    niter = niter,
+    analyses = analyses,
+    convergence = convergence)
+
+#  for (rep in 2:nrep) {
+#    object <- update_jags(object, nrep = 1)
+#  }
   #   
   #   object <- calc_power(object, pars = pars)
-  #   
-  #   return (object)
+  # 
+  
+  class(object) <- "jags_power"
+
+  return (object)
 }
