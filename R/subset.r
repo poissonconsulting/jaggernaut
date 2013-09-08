034195ca8a2b31ff75d676a4fa9c60b50334bf48
Joe Thorley <joe@poissonconsulting.ca>
2013-09-08 13:49:49 -0700

    subset.jags_analysis now calls subset_jags.jags_analysis

--- R/subset.r
+++ R/subset.r
@@ -1,38 +1,4 @@
 
 subset.jags_analysis <- function (x, model_number = 0, ...) {   
-
-  if (!is.jags_analysis(x)) {
-    stop("x must be a jags_analysis")
-  }
-  
-  if (is.numeric(model_number)) {
-    if (length(model_number) != 1) {
-      stop("model_number must be a single value")
-    }
-    if (model_number < 0 || model_number > nmodel(x)) {
-      stop(paste("model_number must lie between 0 and the number of models (in this case",nmodel(x),")"))
-    }
-  } else {
-    stop ("model_number must be an integer")
-  }
-  
-  model_number <- as.integer(model_number)
-  
-  if (nmodel(x) == 1)
-    return (x)
-  
-  newObject <- list()
-  newObject$analyses <- list()
-  if(model_number == 0) {
-    newObject$analyses[[1]] <- x$analyses[[rownames(x$dic)[1]]]
-    
-  } else {
-    newObject$analyses[[1]] <- x$analyses[[model_number]]
-  }
-  newObject$dic <- x$dic[rownames(x$dic) == paste0("Model",model_number),,drop=T]
-  newObject$n.model <- 1
-  
-  class(newObject) <- "jags_analysis"
-  
-  return (newObject)
+  return (subset_jags(x, model_number = model_number))
 }
