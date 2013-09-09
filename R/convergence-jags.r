1c9156fe16f7e55e165cb4f374925306d477face
Joe Thorley <joe@poissonconsulting.ca>
2013-09-09 14:52:15 -0700

    added ...

--- R/convergence-jags.r
+++ R/convergence-jags.r
@@ -1,12 +1,12 @@
 
 #' @export
-convergence_jags <- function (object, parm = NULL) {
-  UseMethod("convergence_jags", object1)
+convergence_jags <- function (object, ...) {
+  UseMethod("convergence_jags", object)
 }
 
 #' @method convergence_jags jags_analysis
 #' @export 
-convergence_jags.jags_analysis <- function (object)
+convergence_jags.jags_analysis <- function (object, ...)
 {
   if(!is.jags_analysis(object))
     stop("object should be of class jags_analysis")
@@ -18,7 +18,7 @@ convergence_jags.jags_analysis <- function (object)
 
 #' @method convergence_jags jags_analysis
 #' @export 
-convergence_jags.jags_power <- function (object)
+convergence_jags.jags_power <- function (object, ...)
 {
   if(!is.jags_analysis(object))
     stop("object should be of class jags_analysis")
