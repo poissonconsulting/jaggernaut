4d6dba8dc788ada2d3d8fbb251a4d08d25ae6504
Joe Thorley <joe@poissonconsulting.ca>
2013-09-10 11:03:36 -0700

    replaced subset with subset_jags

--- R/subset-jags.r
+++ R/subset-jags.r
@@ -1,15 +1,135 @@
+
+#' @title Subset a JAGS object
+#'
+#' @description
+#' Subsets a JAGS object.  
+#' 
+#' @param object a JAGS object.
+#' @param ... other arguments.
+#' @return an object of the same JAGS class.
+#' @seealso \code{\link{subset_jags.jags_analysis}},
+#' \code{\link{subset_jags.jags_simulation}} and \code{\link{subset_jags.jags_power_analysis}}
 #' @export
-subset_jags <- function (x, ...) {
-  UseMethod("subset_jags", x)
+subset_jags <- function (object, ...) {
+  UseMethod("subset_jags", object)
 }
 
+#' @title Subset a JAGS model
+#'
+#' @description
+#' Subset a JAGS model object.  
+#' 
+#' @param object a jags_model object.
+#' @param models an integer element indicating the model(s) to select.
+#' @param ... other arguments passed to generic function.
+#' @return the subsetted jags_analysis object
+#' @seealso \code{\link{subset_jags}} and \code{\link{jags_analysis}}
+#' @examples
+#' model1 <- jags_model("
+#' model { 
+#'  bLambda ~ dlnorm(0, 10^-2) 
+#'  for (i in 1:nrow) { 
+#'    x[i]~dpois(bLambda) 
+#'  } 
+#'}")
+#'
+#' model2 <- jags_model("
+#' model { 
+#'  bLambda ~ dnorm(0, 10^-2) 
+#'  sLambda ~ dunif(0, 5)
+#'  for (i in 1:nrow) { 
+#'    x[i] ~ dnorm(bLambda, sLambda^-2) 
+#'  } 
+#'}")
+#'
+#' models <- add_jags(model1, model2, model1)
+#' data <- data.frame(x = rpois(100,10))
+#' 
+#' analysis <- jags_analysis (models, data, mode = "demo")
+#' 
+#' summary(analysis)
+#' summary(subset_jags(analysis))
+#' summary(subset_jags(analysis, model_number = 2))
+#' 
+#' @method subset_jags jags_model
+#' @export 
+subset_jags.jags_model <- function (object, model = NULL, ...) {   
+  
+  if(is.null(model))
+    return (object)
+  
+  if (!is.numeric(model)) 
+    stop("model must be an integer vector")
+
+  if (length(model) == 0)
+    stop("model must have at least one value")
+
+  if (any(is.na(model)))
+    stop("model must not include missing values")
+  
+  model <- as.integer(model)
+  
+  if (max(model) > nmodel(object))
+    stop("model must be less than the number of models in object")  
+  
+  if (min(model) < 1)
+    stop("model must be less positive")  
+  
+  object$models <- object$models[model]
+  object$nmodel <- length(object$models)
+  
+  return (object)
+}
+
+#' @title Subset a JAGS analysis
+#'
+#' @description
+#' Subset a JAGS analysis object.  
+#' 
+#' @param object a jags_analysis object.
+#' @param model_number an integer element indicating the model to select.
+#' @param ... other arguments passed to generic function.
+#' @return the subsetted jags_analysis object
+#' @seealso \code{\link{subset_jags}} and \code{\link{jags_analysis}}
+#' @examples
+#' model1 <- jags_model("
+#' model { 
+#'  bLambda ~ dlnorm(0, 10^-2) 
+#'  for (i in 1:nrow) { 
+#'    x[i]~dpois(bLambda) 
+#'  } 
+#'}")
+#'
+#' model2 <- jags_model("
+#' model { 
+#'  bLambda ~ dnorm(0, 10^-2) 
+#'  sLambda ~ dunif(0, 5)
+#'  for (i in 1:nrow) { 
+#'    x[i] ~ dnorm(bLambda, sLambda^-2) 
+#'  } 
+#'}")
+#'
+#' models <- add_jags(model1, model2, model1)
+#' data <- data.frame(x = rpois(100,10))
+#' 
+#' analysis <- jags_analysis (models, data, mode = "demo")
+#' 
+#' summary(analysis)
+#' summary(subset_jags(analysis))
+#' summary(subset_jags(analysis, model_number = 2))
+#' 
 #' @method subset_jags jags_analysis
 #' @export 
-subset_jags.jags_analysis <- function (x, model_number = 0, ...) {   
+subset_jags.jags_analysis <- function (object, model = NULL, ...) {   
   
-  if (!is.jags_analysis(x)) {
-    stop("x must be a jags_analysis")
-  }
+  if(is.null(model))
+    return (object)
+  
+  x <- object
+  rm(object)
+  
+  model_number <- model
+  rm(model)
   
   if (is.numeric(model_number)) {
     if (length(model_number) != 1) {
@@ -43,11 +163,48 @@ subset_jags.jags_analysis <- function (x, model_number = 0, ...) {
   return (newObject)
 }
 
+#' @title Subset a JAGS simulation
+#'
+#' @description
+#' Subset a JAGS simulation object.  
+#' 
+#' @param object a jags_simulation object.
+#' @param value an integer vector indicating the rows in values to select.
+#' @param rep an integer vector indicating the replicates to select.
+#' @param ... other arguments passed to generic function.
+#' @return the subsetted JAGS simulation object.
+#' @seealso \code{\link{subset_jags}} and \code{\link{jags_simulation}}
+#' #' @examples
+#' 
+#' data_model <- jags_data_model("
+#' data { 
+#'  for (i in 1:nx) { 
+#'    x[i] ~ dpois(bIntercept) 
+#'    for (j in 1:nx) {
+#'      y[i,j] ~ dpois(bIntercept) 
+#'    }
+#'  } 
+#'  z <- bIntercept
+#'}    
+#' ")
+#'
+#' values <- data.frame(nx = c(1,10), bIntercept = c(5,10))
+#' 
+#' simulation <- jags_simulation (data_model, values, nrep = 5, mode = "test")
+#' 
+#' subset_jags(simulation)
+#' subset_jags(simulation, value = 1, rep = NULL)
+#' subset_jags(simulation, value = NULL, rep = 1)
+#' subset_jags(simulation, value = 1, rep = 1)
 #' @method subset_jags jags_simulation
 #' @export 
-subset_jags.jags_simulation <- function (x, rep = 1, value = 1, ...) {   
-  if(!is.jags_simulation(x))
-    stop("x should be of class jags_simulation")
+subset_jags.jags_simulation <- function (object, value = NULL, rep = NULL, ...) {   
+
+  if(is.null(rep) & is.null(value))
+    return (object)
+  
+  x <- object
+  rm(object)
   
   if(!(is.null(value))) {
     if(!is.numeric(value))
@@ -89,10 +246,12 @@ subset_jags.jags_simulation <- function (x, rep = 1, value = 1, ...) {
   return (x)
 }
 
-#' @method subset_jags jags_power_analysis
-#' @export 
-subset_jags.jags_power_analysis <- function (x, rep = 1, value = 1, ...) {   
+subset_jags.jags_power_analysis <- function (object, model = NULL, value = NULL, rep = NULL, ...) {
+  
+  if(is.null(model) & is.null(rep) & is.null(value))
+    return (object)
+  
   stop("not yet implemented")
   
-  return (x)
+  return (object)
 }
--- R/subset.r
+++
@@ -1,4 +0,0 @@
-
-subset.jags_analysis <- function (x, model_number = 0, ...) {   
-  return (subset_jags(x, model_number = model_number))
-}
