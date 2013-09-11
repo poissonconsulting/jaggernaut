f670170b9444c1de4faddee34cb9f60ec8b877a0
Joe Thorley <joe@poissonconsulting.ca>
2013-09-11 10:42:25 -0700

    mods

--- R/rhat.r
+++ R/rhat.r
@@ -13,9 +13,18 @@ rhat <- function (object, ...) {
   UseMethod("rhat", object)
 }
 
+#' @method rhat jags_mcmc
+rhat.jags_mcmc <- function (object, parm = NULL, ...)
+{
+  if (!is.null(parm))
+    stop("not yet implemented")
+  
+  return (min(object$rhat[,1]))
+}
+
 #' @method rhat jags_analysis
 #' @export 
-rhat.jags_analysis <- function (object, model = NULL, ...)
+rhat.jags_analysis <- function (object, model = NULL, parm = NULL, ...)
 {
   stop("not yet implemented")
   
@@ -24,7 +33,7 @@ rhat.jags_analysis <- function (object, model = NULL, ...)
 
 #' @method rhat jags_power_analysis
 #' @export 
-rhat.jags_power_analysis <- function (object, model = NULL, value = NULL, rep = NULL, ...)
+rhat.jags_power_analysis <- function (object, model = NULL, value = NULL, rep = NULL, parm = NULL, ...)
 {
   stop("not yet implemented")
   
