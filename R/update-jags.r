3c90e8a1770c9e00bdee636f9e6362a3f3a00024
Joe Thorley <joe@poissonconsulting.ca>
2013-09-11 10:42:45 -0700

    moving update to update_jags

--- R/update-jags.r
+++ R/update-jags.r
@@ -4,6 +4,81 @@ update_jags <- function (object, ...) {
   UseMethod("update_jags", object)
 }
 
+#' @method updated jags_mcmc
+update_jags.jags_mcmc <- function (object, ...)
+{
+
+}
+
+#' @method update_jags jagr_analysis
+#' @export 
+update_jags.jagr_analysis <- function (object, quiet = F, ...)
+{  
+  fun2 <- function (jags, monitor, n.sim, n.thin, quiet, recompile)
+  {
+    n.sim <- as.integer(n.sim)
+    n.thin <- as.integer(n.thin)
+    
+    if (quiet) {
+      options(jags.pb = "none")
+    } else {
+      options(jags.pb = "text")
+    }
+    
+    if (recompile)
+      jags$recompile()
+    
+    mcmc <- jags.samples(
+      model = jags, variable.names = monitor, n.iter = n.sim, thin = n.thin
+    )
+    
+    mcmc <- jags_mcmc(mcmc=mcmc,jags=list(jags))
+    return (mcmc)
+  }
+  
+  n.chain <- nchain(object)
+  n.sim <- object$iterations
+  n.thin <- max(1, floor(n.chain * n.sim / nsim(object)))
+  
+  monitor <- object$model$monitor
+  
+  jags <- object$mcmc$jags
+  
+  parallel <- length(jags) > 1
+  
+  ptm <- proc.time()
+  
+  
+  if (parallel) {
+    
+    doMC::registerDoMC(cores=n.chain)   
+    i <- 1 # hack to prevent warning on package check
+    mcmc <- foreach::foreach(i = 1:n.chain, .combine = add_jags_jags_mcmc) %dopar% {
+      fun2(
+        jags = jags[[i]], monitor = monitor, n.sim = n.sim, n.thin = n.thin, 
+        quiet = quiet, recompile = T
+      )
+    }
+  } else {    
+    mcmc <- fun2 (
+      jags = jags[[1]], monitor = monitor, n.sim = n.sim, n.thin = n.thin, 
+      quiet = quiet, recompile = F
+    )
+  }
+  object$mcmc <- mcmc
+  object$iterations <- object$iterations * 2
+  object$time <- object$time + ((proc.time () - ptm)[3]) / (60 * 60)
+  
+  return (object)
+}
+
+#' @method update_jags jags_analysis
+#' @export 
+update_jags.jags_analysis <- function (object, quiet = F, ...)
+{
+  
+}
+
 #' @method update_jags jags_simulation
 #' @export 
 update_jags.jags_simulation <- function (object, nrep = 1, values = NULL, mode = "current", ...)
@@ -43,15 +118,15 @@ update_jags.jags_simulation <- function (object, nrep = 1, values = NULL, mode =
   on.exit(opts_jagr(old_opts))
   
   if(!is.null(values)) {
-  
+    
     if(any(!colnames(object$values) %in% colnames(values)))
       stop("colnames missing from values")
-  
+    
     if(any(!colnames(values) %in% colnames(object$values)))
       warning("unrecognised colnames in values")  
-  
+    
     values <- subset(values, select = colnames(object$values))
-
+    
     newObject <- jags_simulation(data_model = object$data_model, 
                                  nrep = object$nrep + nrep, 
                                  values = values, 
@@ -80,4 +155,11 @@ update_jags.jags_simulation <- function (object, nrep = 1, values = NULL, mode =
   #  object <- object + newObject
   
   return (object)
+}
+
+#' @method update_jags jags_power_analysis
+#' @export 
+update_jags.jags_power_analysis <- function (object, quiet = F, ...)
+{
+  
 }
