102c3b4f999335c6bc11a9d067e0ac414fcc00fd
Joe Thorley <joe@poissonconsulting.ca>
2013-09-23 17:13:04 -0700

    now uses access functions

--- R/expand-parm.r
+++ R/expand-parm.r
@@ -1,13 +1,15 @@
 
-expand_parm <- function (object, parm = "all") {
-  stopifnot(is.jags_jagr_analysis(object))
+expand_parm <- function (object, parm, ...) {
+  UseMethod("expand_parm", object)
+}
+
+expand_parm.jags_model <- function (object, parm = "all", ...) {
+  
   stopifnot(is.character(parm) && is_length(parm) && is_defined(parm))
-  
-  model <- as.jags_model(object)
-  
+      
   parm <- unique(parm)
   
-  pars <- model$monitor
+  pars <- monitor(object)
   
   if("all" %in% parm || all(c("fixed","random") %in% parm)) {
     return (pars)
@@ -16,12 +18,40 @@ expand_parm <- function (object, parm = "all") {
   add <- NULL
   
   if ("fixed" %in% parm) {
-    add <- pars[!pars %in% names(model$random)]
+    add <- pars[!pars %in% names(random_effects(object))]
   } else if ("random" %in% parm) {
-    add <- pars[pars %in% names(model$random)]
+    add <- pars[pars %in% names(random_effects(object))]
   }
   pars <- pars[pars %in% parm]
   pars <- unique(c(pars,add))  
   
   return (pars)
+}
+
+expand_parm.jagr_analysis <- function (object, parm = "all", ...) {
+
+  stopifnot(is.character(parm) && is_length(parm) && is_defined(parm))
+  
+  return (expand_parm(as.jags_model(object), parm = parm, ...))
+}
+
+expand_parm_jagr_analysis <- function (object, parm = "all", ...) {
+  
+  stopifnot(is.jagr_analysis(object))
+  
+  return (expand_parm(object, parm = parm, ...))
+}
+
+expand_parm.jags_analysis <- function (object, parm = "all", ...) {
+  
+  stopifnot(is.character(parm) && is_length(parm) && is_defined(parm))
+  
+  object <- as.jagr_analysis(object)
+    
+  if(is.jagr_analysis(object))
+    return (expand_parm(object, parm = parm, ...))
+    
+  parm <- lapply(object, expand_parm_jagr_analysis, parm = parm, ...)
+  
+  return (parm)
 }
