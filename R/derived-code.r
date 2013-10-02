1ba1a2468b8bbda44ab220c262fc489b3de4a36b
Joe Thorley <joe@poissonconsulting.ca>
2013-10-01 18:28:09 -0700

    separated derived_code and random_effects from jags_model

--- R/derived-code.r
+++ R/derived-code.r
@@ -9,60 +9,56 @@ derived_code <- function (object, ...) {
 
 derived_code.jags_model <- function (object, ...) {
   
-  x <- list()
-  for (i in 1:length(object$models))
-    x[[i]] <- object$models[[i]]$derived_model
-  x <- delist(x)
-
-  if (length(x) == 0)
+  object <- object$derived_code
+  
+  object <- delist(object)
+  
+  if(length(object) == 1 && is.na(object))
     return (NULL)
   
-  return (x)
-}
-
-derived_code.jagr_analysis <- function (object, ...) {
-  return (derived_code(as.jags_model(object), ...))
-}
-
-derived_code_jagr_analysis <- function (object, ...) {
-  stopifnot(is.jagr_analysis(object))
-  return (derived_code(object, ...))
+  return (object)
 }
   
 derived_code.jags_analysis <- function (object, ...) {
-  object <- as.jagr_analysis(object)
-  if (is.jagr_analysis(object))
-    return (derived_code(object, ...))
-  
-  object <- lapply(object, derived_code_jagr_analysis, ...)
-  
-  return (object)
+  return (derived_code(as.jags_model(object), ...))
 }  
 
 "derived_code<-.jags_model" <- function (object, value, ...) {
   
   if (!is.null (value)) {
     if(!is.character(value)) {
-      stop("derived_code must be a character")
+      stop("derived_code must be NULL or a character")
     }
     if (length(value) != 1) {
       stop ("derived_code must be define a single model block")
     }
   }
   
-  for (i in 1:length(object$models))
-    object$models[[i]]$derived_model <- value
-  return (object)
-}
-
-"derived_code<-.jagr_analysis" <- function (object, value, ...) {
-  derived_code (object$model, ...) <- value
+  if(is.null(value))
+    is.na(value) <- TRUE
+    
+  for (i in 1:nmodel(object))
+    object$derived_code[[i]] <- value
+  
   return (object)
 }
 
 "derived_code<-.jags_analysis" <- function (object, value, ...) {
-  for (i in 1:length(object$analyses)) {
-    derived_code (object$analyses[[1]], ...) <- value
+  
+  if (!is.null (value)) {
+    if(!is.character(value)) {
+      stop("derived_code must be NULL or a character")
+    }
+    if (length(value) != 1) {
+      stop ("derived_code must be define a single model block")
+    }
   }
+  
+  if(is.null(value))
+    is.na(value) <- TRUE
+  
+  for (i in 1:nmodel(object))
+    object$derived_code[[i]] <- value
+  
   return (object)
 }
