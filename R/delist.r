56129b5a229a022c7d532d8d4016f7071b588613
Joe Thorley <joe@poissonconsulting.ca>
2013-10-02 10:05:36 -0700

    uses is_named instead of is.data.frame and all jags_ classes

--- R/delist.r
+++ R/delist.r
@@ -2,13 +2,6 @@
 delist <- function (object) {
   
   if (!is.list(object) 
-      || is.data.frame(object) 
-      || is.jagr_model(object)
-      || is.jags_model(object)      
-      || is.jags_data_model(object) 
-      || is.jagr_analysis(object)       
-      || is.jags_analysis(object)       
-      || is.jags_simulation(object) 
       || is_named(object)
       || length(object) != 1)
     return (object)
