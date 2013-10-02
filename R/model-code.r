a35a5cd3826c300c697f03eb1cdda93857c9c2b0
Joe Thorley <joe@poissonconsulting.ca>
2013-10-02 08:52:27 -0700

    updated

--- R/model-code.r
+++ R/model-code.r
@@ -7,36 +7,72 @@ model_code <- function (object, ...) {
   UseMethod("model_code<-", object)
 }
 
+model_code.jagr_model <- function (object, ...) {
+  return (object$model_code)
+}
+
+model_code_jagr_model <- function (object, ...) {
+  stopifnot(is.jagr_model(object))
+  return (model_code(object, ...))
+}
+
 model_code.jags_model <- function (object, ...) {
   
-  x <- character()
-  for (i in 1:length(object$models))
-    x[[i]] <- object$models[[i]]$model_code
-  return (x)
+  object <- as.jagr_model(object)
+  
+  if(is.jagr_model(object))
+    return (model_code(object, ...))
+  
+  object <- lapply(object, model_code_jagr_model, ...)
+  
+  object <- delist(object)
+  return (object)  
+}
+
+model_code.jags_data_model <- function (object, ...) {
+  return (model_code(as.jags_model(object), ...))
 }
 
 model_code.jagr_analysis <- function (object, ...) {
-  return (model_code(as.jags_model(object), ...))
+  return (model_code(as.jagr_model(object), ...))
+}
+
+model_code_jagr_analysis <- function (object, ...) {
+  stopifnot(is.jagr_analysis(object))
+  return (model_code(object, ...))
 }
 
 model_code.jags_analysis <- function (object, ...) {
   return (model_code(as.jags_model(object), ...))
 }
 
-"model_code<-.jags_model" <- function (object, value, ...) {
-  
-  if(is.null(value))
-    return (object)
+"model_code<-.jagr_model" <- function (object, value, ...) {
   
   if (is.character (value)) {
     if (length(value) != 1) {
-      stop ("model_code must be define a single model - for multiple models use add_jags to add multiple jags_models")
+      stop ("model_code must define a single model - for multiple models use add_jags to add multiple jags_models")
     }
   } else {
     stop ("model_code must be class character")
   }
   
-  for (i in 1:length(object$models))
-    object$models[[i]]$model_code <- value
+  object$model_code <- value
+  
+  return (object)
+}
+
+"model_code<-.jags_model" <- function (object, value, ...) {
+  
+  for (i in 1:nmodel(object))
+    model_code(object$models[[i]], ...) <- value
+  
+  return (object)
+}
+
+"model_code<-.jags_data_model" <- function (object, value, ...) {
+  
+  for (i in 1:nmodel(object))
+    model_code(object$models[[i]], ...) <- value
+  
   return (object)
 }
