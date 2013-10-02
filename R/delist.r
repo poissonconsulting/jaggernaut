ce7cd33824b2734d2ca906a0104677cd70829fe6
Joe Thorley <joe@poissonconsulting.ca>
2013-10-02 08:51:04 -0700

    now not worked on named lists of those of jags class

--- R/delist.r
+++ R/delist.r
@@ -1,7 +1,16 @@
 
 delist <- function (object) {
   
-  if (!is.list(object) || is.data.frame(object) || length(object) != 1)
+  if (!is.list(object) 
+      || is.data.frame(object) 
+      || is.jagr_model(object)
+      || is.jags_model(object)      
+      || is.jags_data_model(object) 
+      || is.jagr_analysis(object)       
+      || is.jags_analysis(object)       
+      || is.jags_simulation(object) 
+      || is_named(object)
+      || length(object) != 1)
     return (object)
   
   object <- object[[1]]
