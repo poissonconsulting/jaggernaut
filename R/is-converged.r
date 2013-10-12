a821480fc1631098809c47f660adf8ca17c42056
Joe Thorley <joe@poissonconsulting.ca>
2013-10-12 14:14:49 -0700

    is_converged.default to replace some functions plus make parm = "all" and combine = TRUE explicit

--- R/is-converged.r
+++ R/is-converged.r
@@ -1,31 +1,24 @@
 
 #' @export
-is_converged <- function (object, ...)
+is_converged <- function (object, ...) {
   UseMethod("is_converged", object)
-
-is_converged.jagr_chains <- function (object, rhat_threshold, ...) {
-  stopifnot(is_scalar(rhat_threshold))
-  rhat(object, parm = "all", combine = TRUE) <= rhat_threshold
 }
 
-is_converged.jagr_power_analysis <- function (object, rhat_threshold, ...)
-  is_converged(as.jagr_chains(object), rhat_threshold = rhat_threshold, ...)
-
-is_converged_jagr_power_analysis <- function (object, rhat_threshold, ...) {
-  stopifnot(is.jagr_power_analysis(object))
-  return (is_converged(object, rhat_threshold = rhat_threshold, ...))
+is_converged.default <- function (object, rhat_threshold, ...) {
+  stopifnot(is_scalar(rhat_threshold))  
+  return (rhat(object, parm = "all", combine = TRUE) <= rhat_threshold) 
 }
 
 #' @method is_converged jags_analysis
 #' @export 
 is_converged.jags_analysis <- function (object, ...) {
-  return (rhat (object, ...) <= rhat_threshold(object))
+  return (rhat (object, parm = "all", combine = TRUE) <= rhat_threshold(object))
 }
 
 #' @method is_converged jags_power_analysis
 #' @export 
 is_converged.jags_power_analysis <- function (object, percent = FALSE, ...) { 
-  rhat <- rhat(object, ...)
+  rhat <- rhat(object, parm = "all", combine = TRUE)
   rhat <- rhat <= rhat_threshold(object)
   if(!percent) {
     return (rhat)
