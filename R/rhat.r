d9ce840b8461686d041252c0788f19e758ec58e1
Joe Thorley <joe@poissonconsulting.ca>
2013-10-12 14:13:37 -0700

    removed ... from rhat and added parm and combine to all

--- R/rhat.r
+++ R/rhat.r
@@ -8,12 +8,11 @@
 #' @param ... passed to and from other functions
 #' @return a vector, matrix or array of rhat values
 #' @export
-rhat <- function (object, ...) {
+rhat <- function (object, parm, combine) {
   UseMethod("rhat", object)
 }
 
-rhat.jagr_chains <- function (object, parm = "all", combine = TRUE, ...)
-{ 
+rhat.jagr_chains <- function (object, parm = "all", combine = TRUE) { 
   stopifnot(is.character(parm) && is_length(parm) && is_defined(parm))
   stopifnot(is_indicator(combine))
   
@@ -34,32 +33,32 @@ rhat.jagr_chains <- function (object, parm = "all", combine = TRUE, ...)
   return (rhat)
 }
 
-rhat.jagr_power_analysis <- function (object, model, parm, combine = TRUE, ...) {
-  return (rhat(chains(object), parm = parm, combine = combine,...))
+rhat.jagr_power_analysis <- function (object, parm = "all", combine = TRUE) {
+  return (rhat(chains(object), parm = parm, combine = combine))
 }
 
-rhat_jagr_power_analysis <- function (object, parm, combine = TRUE, ...) {
+rhat_jagr_power_analysis <- function (object, parm = "all", combine = TRUE) {
   stopifnot(is.jagr_power_analysis(object))
-  return (rhat(object, parm = parm, combine = combine, ...))
+  return (rhat(object, parm = parm, combine = combine))
 }
 
-rhat.jagr_analysis <- function (object, parm = "all", combine = TRUE, ...) {
+rhat.jagr_analysis <- function (object, parm = "all", combine = TRUE) {
   stopifnot(is.character(parm) && is_length(parm) && is_defined(parm))
   stopifnot(is_indicator(combine))
   
   parm <- expand_parm(object, parm = parm)
   
-  return (rhat(as.jagr_chains(object), parm = parm, combine = combine,...))
+  return (rhat(as.jagr_chains(object), parm = parm, combine = combine))
 }
 
-rhat_jagr_analysis <- function (object, parm = "all", combine = TRUE...) {
+rhat_jagr_analysis <- function (object, parm = "all", combine = TRUE) {
   stopifnot(is.jagr_analysis(object))
-  return (rhat(object, parm = parm, combine = combine, ...))
+  return (rhat(object, parm = parm, combine = combine))
 }
 
 #' @method rhat jags_analysis
 #' @export 
-rhat.jags_analysis <- function (object, parm = "all", combine = TRUE, ...) {
+rhat.jags_analysis <- function (object, parm = "all", combine = TRUE) {
   if(!is.character(parm))
     stop("parm must a character vector")
 
@@ -75,25 +74,25 @@ rhat.jags_analysis <- function (object, parm = "all", combine = TRUE, ...) {
   if(is_one_model(object))
     return (rhat(analysis(object), 
                  parm = parm, 
-                 combine = combine, ...))
+                 combine = combine))
   
   analyses <- analyses(object)
   analyses <- lapply(analyses, 
                      rhat_jagr_analysis, 
                      parm = parm, 
-                     combine = combine, ...)  
+                     combine = combine)  
   analyses <- name_object(analyses, "model")
   return (models) 
 }
 
 #' @method rhat jags_power_analysis
 #' @export 
-rhat.jags_power_analysis <- function (object, parm = "all", combine = TRUE, ...) {
-  lapply_rhat_jagr_power_analysis <- function (object, model,
-                                         parm, combine, ...) {    
+rhat.jags_power_analysis <- function (object, parm = "all", combine = TRUE) {
+
+  lapply_rhat_jagr_power_analysis <- function (object, parm, combine) {    
     return (lapply(object, rhat_jagr_power_analysis, 
                    parm = parm, 
-                   combine = combine, ...))
+                   combine = combine))
   }
   
   parm <- expand_parm(object, parm)
@@ -102,10 +101,10 @@ rhat.jags_power_analysis <- function (object, parm = "all", combine = TRUE, ...)
     
   rhat <- lapply(analyses, lapply_rhat_jagr_power_analysis, 
                  parm = parm, 
-                 combine = combine, ...)
+                 combine = combine)
 
   if(combine) {
-    rhat <- arrayicise(rhat)
+    rhat <- matrixise(rhat)
     rhat <- name_object(t(rhat),c("replicate","value"))
   } else
     rhat <- name_object(rhat,c("value","replicate"))
