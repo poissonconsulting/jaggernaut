72fde851c3ce83cd61b4236d310b71c3f16e2dc0
Joe Thorley <joe@poissonconsulting.ca>
2013-10-14 15:03:21 -0700

    plyr::ddply

--- R/coef.r
+++ R/coef.r
@@ -186,7 +186,7 @@ coef.jags_power_analysis <- function (object, parm = "fixed", combine = FALSE, l
                        significance = significance))
   }
   
-  coef <- ddply(coef, .(value,parameter), get_estimates, power_level = power_level)
+  coef <- plyr::ddply(coef, .(value,parameter), get_estimates, power_level = power_level)
   
   values <- values(object)
   values <- cbind(data.frame(value = row.names(values)),values)
