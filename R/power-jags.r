b6f53411af7a2d79b9da77d43431142a0a08ecd5
Joe Thorley <joe@poissonconsulting.ca>
2013-10-16 15:37:56 -0700

    power_jags working!

--- R/power-jags.r
+++ R/power-jags.r
@@ -1,17 +1,14 @@
                                          
 #' @export
-power_jags <- function (object, parm = list("fixed" = c("significance < 0.05")), level = "current") {
+power_jags <- function (object, parm = c(fixed = 0), level = "current") {
   if(!is.jags_power_analysis(object))
     stop("object must be a jags_power_analysis")
   
-  if(!(is.list(parm) && is_named(parm)))
-    stop("parm must be a named list")
+  if(!(is.numeric(parm) && is_named(parm)))
+    stop("parm must be a named numeric vector")
 
   if(length(parm) == 0)
     stop("parm must contain at least one element")
-  
-  if(!all(sapply(parm, is_character)) || !all(sapply(parm, is_scalar)))
-    stop("all the elements in parm must be character scalars")
        
   old_opts <- opts_jagr()
   on.exit(opts_jagr(old_opts))
@@ -27,26 +24,9 @@ power_jags <- function (object, parm = list("fixed" = c("significance < 0.05")),
   parms <- parms[-1,]
   
   for (i in seq_along(parm)) {
-    df <- data.frame(parameter = expand_parm(object, names(parm)[i]))
-                         
-    str <- strsplit(parm[[1]]," ", fixed =TRUE)[[1]]
+    df <- data.frame(parameter = expand_parm(object, names(parm)[i], indices = TRUE))
     
-    if(length(str) != 3)
-      stop("the elements of parm must use syntax of the form 'p < 0.05' where there
-           is a space between the statistic, comparison operator and bound")
-    
-    if(!str[1] %in% c("estimate","lower","upper","sd","error","significance"))
-      stop("the statistics in parm must be 'estimate','lower','upper',
-            'sd','error' or 'significance'")
-
-    if(!str[2] %in% c("<","<=",">",">="))
-      stop("the comparison operators in parm must be '<','<=','>',
-            or '>='")
-    if(is.na(is.numeric(str[3])))
-      stop("the bounds in parm must be numeric values")   
-                     
-    df2 <- data.frame(statistic = str[1], comparison = str[2], bound = str[3])
-    df <- merge(df, df2)
+    df$bound <- parm[i]
     
     parms <- rbind(parms, df)
   }
@@ -72,8 +52,8 @@ power_jags <- function (object, parm = list("fixed" = c("significance < 0.05")),
   ldply_analyses <- function (x, parm, level, rhat_threshold) {
     return (plyr::ldply(x, melt_coef, parm, level, rhat_threshold))
   }
-      
-  coef <- plyr::ldply(analyses, ldply_analyses, parm = parms$parameter, level = level, rhat_threshold = rhat_threshold)
+        
+  coef <- plyr::ldply(analyses, ldply_analyses, parm = expand_parm(object, names(parm)), level = level, rhat_threshold = rhat_threshold)
   
   coef$replicate <- paste0("replicate",as.integer(substr(coef$.id,10,15)))
   coef$value <- paste0("value",rep(1:nvalues(object), each = nrow(coef)/nvalues(object)))
@@ -86,26 +66,30 @@ power_jags <- function (object, parm = list("fixed" = c("significance < 0.05")),
   values <- cbind(data.frame(value = row.names(values)),values)
   
   parms <- merge(values, parms)
+  
   power <- merge(parms, coef)
+      
+  get_power <- function (d) {
     
-  power$converged <- NA
-  power$nreps <- NA
+    bound <- d$bound[1]
+    lower <- d[d$statistic == "lower",substr(colnames(d),1,9) == "replicate"]
+    upper <- d[d$statistic == "upper",substr(colnames(d),1,9) == "replicate"]
+    converged <- round(length(lower[!is.na(lower)]) / length(lower),2)
+    samples <- length(lower[!is.na(lower)])
+    bol <- !is.na(lower) & (lower > bound | upper < bound)
+    out <- length(bol[bol])
+    return (data.frame(bound = bound, converged = converged, samples = samples, out = out))
+  }
+  power <- plyr::ddply(power,plyr::.(value,parameter,bound), get_power)
   power$power <- NA
   power$lower <- NA
   power$upper <- NA
   
-  for(i in 1:nrow(power)) {
-    vec <- power[i,substr(colnames(power),1,9) == "replicate"]
-    power$converged[i] <- round(length(vec[!is.na(vec)]) / length(vec),2)
-    power$nreps[i] <- length(vec[!is.na(vec)])
-    cmd <- paste("power$estimate[i] <- length(vec[!is.na(vec) & vec",power$comparison[i],power$bound[i],"])")
-    eval(parse(text = cmd))
-  }
-    
-  power[,c("estimate","lower","upper")] <- binomjags(n = power$nreps, s = power$estimate)
-  power$power <- power$estimate
+  power[,c("power","lower","upper")] <- binomjags(n = power$samples, s = power$out)
   
-  power <- subset(power,select = c(colnames(values),"parameter","statistic","comparison","bound","converged","nreps","power","lower","upper"))
+  power <- merge(values, power, by = "value")
+  
+  power <- subset(power,select = c(colnames(values),"parameter","bound","converged","samples","out","power","lower","upper"))
   
   return (power)
 }
