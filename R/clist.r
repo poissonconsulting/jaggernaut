ac12f3a6bf5c01b7b48eb37e281f2f1d6309246a
Joe Thorley <joe@poissonconsulting.ca>
2013-10-19 18:30:18 -0700

    made clist recursive

--- R/clist.r
+++ R/clist.r
@@ -1,20 +1,33 @@
 
-clist <- function (object, object2) {
+clist <- function (object, object2, recursive = 1) {
+  
+  recursive <- as.integer(recursive)
 
   stopifnot(is.list(object))
   stopifnot(is.list(object2))
-
-  names <- names(object)
-  names2 <- names(object2)
+  stopifnot(is.integer(recursive))
+  stopifnot(recursive %in% 1:2)
   
-  len <- length(object)
-  for (i in seq_along(object2)) {
-    object[[i + len]] <- object2[[i]]
+  if(recursive == 1) {
+    names <- names(object)
+    names2 <- names(object2)
+    
+    len <- length(object)
+    for (i in seq_along(object2)) {
+      object[[i + len]] <- object2[[i]]
+    }
+    
+    if(!is.null(names) && !is.null(names2)) {
+      names(object) <- c(names, names2)
+    } else
+      names(object) <- NULL
+  } else { 
+    stopifnot(recursive == 2)
+    stopifnot(length(object) == length(object2))
+    
+    for (i in 1:length(object)) {
+      object[[i]] <- clist(object[[i]], object2[[i]])
+    }
   }
-
-  if(!is.null(names) && !is.null(names2)) {
-    names(object) <- c(names, names2)
-  } else
-    names(object) <- NULL
   return (object)
 }
