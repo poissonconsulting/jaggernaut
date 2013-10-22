d2d5d7b1663abd21f39d8eac1d5e55a429c58616
Joe Thorley <joe@poissonconsulting.ca>
2013-10-22 12:46:32 -0700

    fixed bug where nFactor could have been deleted before Factor reached which would have meant not treated as a factor

--- R/zero-random.r
+++ R/zero-random.r
@@ -24,27 +24,30 @@ zero_random.jagr_chains <- function (object, random_variable, ...) {
 
 zero_random.jagr_power_analysis <- function (object, data) {
   stopifnot(is.list(data))
+  
+  names_data <- names_data(data)
       
-  for (name in names_data(data)) {
+  for (name in names_data) {
     var <- data[[name]]
     va <- unique(var)
-    if (!(paste0("n",name) %in% names_data(data) && length(va) == 1 && va == 1))
+    if (!(paste0("n",name) %in% names_data && length(va) == 1 && va == 1))
       data[[name]] <- NULL
   }
   
   random_effects <- random_effects(object)
-      
+    
   for (ran in names(random_effects))
     if (!any(random_effects[[ran]] %in% names_data(data)))
       random_effects[[ran]] <- NULL
-      
+  
   if (length(random_effects))
     return (zero_random (chains(object), names_random = names(random_effects)))
-  
+
   return (chains(object))
 }
 
 zero_random.jags_analysis <- function (object, data, ...) {
   stopifnot(is_one_model(object))
+  
   return (zero_random (analysis(object), data, ...))
 }
