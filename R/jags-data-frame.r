8a7e82f73d0dc65c11805963b5aed4dcdf5c7fc5
Joe Thorley <joe@poissonconsulting.ca>
2013-11-24 15:26:41 -0600

    updated checking and added tests for jags_data_list and jags_data_frame

--- R/jags-data-frame.r
+++ R/jags-data-frame.r
@@ -1,27 +1,14 @@
 
 jags_data_frame <- function (data) {
   
-  if (!is.data.frame(data)) {
+  if (!is.data.frame(data))
     stop("data must be a data.frame")
-  }
   
   object <- data
-  class(object) <- c("jags_data_frame","data.frame","jags_data_list")
+  class(object) <- c("jags_data_frame", "data.frame", "jags_data_list")
   ntries(object) <- 1
-      
-  data <- as.list(data)
   
-  if(length(data) > 0) {
-    
-    bol <- sapply(data, inherits, "logical")
-    
-    for (class in c("integer","numeric","factor","Date","POSIXt"))
-      bol <- bol | sapply(data, inherits, class)
-    
-    if(!all(bol))
-      stop("all elements in data must be class integer, numeric, factor, Date or 
-         POSIXt")
-  }
+  jags_data_list(data)
   
   return (object)
 }
--- R/jags-data-list.r
+++ R/jags-data-list.r
@@ -1,28 +1,28 @@
 
 jags_data_list <- function (data) {
   
-  if (!is.list(data)) {
+  if (!is.list(data))
     stop("data must be a list")
-  }
   
   data <- as.list(data)
+  
+  if (length(data) == 0)
+    stop("data must be a list with at least one element")
+  
+  if (is.null(names(data)) || any(names(data) == "" | duplicated(names(data))))
+    stop("data must be a list of uniquely named elements")
     
-  if(length(data) > 0) {
-    
-    if(is.null(names(data)))
-      stop("data must be a named list")
-    if (any(names(data) == ""))
-      stop("all data elements must be named")
-    
-    bol <- sapply(data, inherits, "logical")
-    
-    for (class in c("integer","numeric","factor","Date","POSIXt","matrix","array"))
-      bol <- bol | sapply(data, inherits, class)
-    
-    if(!all(bol))
-      stop("all elements in data must be class integer, numeric, factor, Date, 
+  if (any(is.null(data)))
+    stop("data must not contain NULL elements")
+  
+  bol <- sapply(data, inherits, "logical")
+  
+  for (class in c("integer","numeric","factor","Date","POSIXt","matrix","array"))
+    bol <- bol | sapply(data, inherits, class)
+  
+  if(!all(bol))
+    stop("elements in data must be class integer, numeric, factor, Date, 
          POSIXt, matrix or array")
-  }
   
   object <- data
   class(object) <- c("jags_data_list")
