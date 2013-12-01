c9503a6897400aa69067537503be77e79254707e
Joe Thorley <joe@poissonconsulting.ca>
2013-12-01 14:18:09 -0600

    added {} to functions

--- R/is.r
+++ R/is.r
@@ -1,33 +1,43 @@
 
-is_scalar <- function (x)
+is_scalar <- function (x) {
   is.vector(x) && length(x) == 1
+}
 
-is_vector <- function (x)
+is_vector <- function (x) {
   is.vector(x) && length(x) > 1
+}
 
-is_length <- function (x)
+is_length <- function (x) {
   is_scalar(x) || is_vector(x)
+}
 
-is_character <- function (x)
+is_character <- function (x) {
   is.character(x)
+}
 
-is_numeric <- function (x)
+is_numeric <- function (x) {
   is.numeric(x)
+}
 
-is_no_missing <- function (x)
+is_no_missing <- function (x) {
   !any(is.na(x))
+}
 
-is_defined <- function (x)
+is_defined <- function (x) {
   is_no_missing(x)
+}
 
-is_indicator <- function (x)
+is_indicator <- function (x) {
  is.logical(x) & is_scalar(x) & is_defined(x)
+}
 
-is_named <- function (x)
+is_named <- function (x) {
   !is.null(names(x)) 
+}
 
-is_one_model <- function (x) 
+is_one_model <- function (x) {
   nmodels(x) == 1
+}
 
 is_data_list <- function (data) {
   if (!is.list(data)) {
