2be1fd7818ab54247eb22ad8f78bc711944a1ada
Joe Thorley <joe@poissonconsulting.ca>
2013-12-02 16:47:33 -0600

    names_data now just names(data)

--- R/names.r
+++ R/names.r
@@ -1,12 +1,5 @@
 
 names_data <- function (data) {
-  
-  if (!(is.data.frame(data) || is_data_list(data)))
-    stop("data must be a data.frame or data list")
-  
-  if(is.data.frame(data)) {
-    return (colnames(data))
-  }
   return (names(data))
 }
 
