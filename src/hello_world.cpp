437b9cd17acb195ec99db0c6ee8ecc0c0c8c0bd8
Joe Thorley <joe@poissonconsulting.ca>
2013-12-06 14:00:44 -0600

    added Rcpp functionality with hello_world!

---
+++ src/hello_world.cpp
@@ -0,0 +1,19 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//' @title Hello world
+//'
+//' @description
+//' You know what this does - its foobar
+//'
+//' @useDynLib jaggernaut
+//' @export
+// [[Rcpp::export]]
+List hello_world() {
+   
+    CharacterVector x = CharacterVector::create( "foo", "bar" )  ;
+    NumericVector y   = NumericVector::create( 0.0, 1.0 ) ;
+    List z            = List::create( x, y ) ;
+    
+    return z ;
+}
