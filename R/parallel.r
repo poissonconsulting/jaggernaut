2330fa173d6528479ed79bc1745d56dd44c913f2
Joe Thorley <joe@poissonconsulting.ca>
2013-12-08 13:43:35 -0600

    added parallel help file

---
+++ R/parallel.r
@@ -0,0 +1,28 @@
+#' @title Run analyses in parallel
+#'
+#' @description 
+#' In jaggernaut analyses can be run in parallel using a registered
+#' parallel backend.
+#' @name parallel
+#' @seealso \code{\link{jags_model}},
+#' \code{\link{jags_analysis}} and \code{\link{opts_jagr}}.
+#' @examples
+#'  
+#' \dontrun{ 
+#' # run peregrine demo using registered parallel backend
+#' registerDoParallel() 
+#' opts_jagr("parallel" = TRUE)
+#' 
+#' demo(woodchat)
+#' 
+#' # stop cluster if registered parallel backend in windows
+#' if(.Platform$OS.type == "windows") stopImplicitCluster()
+#' 
+#' opts_jagr("parallel" = FALSE)
+#' registerDoSEQ()
+#' 
+#' # further information on the woodchat dataset and demo
+#' ?woodchat
+#' }
+#'
+NULL
