399ca9d9cb22ba2ee9f9c416cd65750928a0b0b4
Joe Thorley <joe@poissonconsulting.ca>
2014-01-28 16:47:45 -0600

    fix )

--- R/merge-jags-samples.r
+++ R/merge-jags-samples.r
@@ -14,16 +14,16 @@ merge_jags_samples <- function (object, by = NULL, fun = sum) {
   
   warning("merge_jags_samples is deprecated by combine.jags_sample in v1.6")
   if(length(object) == 1) {
-    return(combine(object[[1]], by = by, fun = fun)
+    return(combine(object[[1]], by = by, fun = fun))
   } 
   if(length(object) == 2) {
-    return(combine(object[[1]], object[[2]], by = by, fun = fun)
+    return(combine(object[[1]], object[[2]], by = by, fun = fun))
   }
   if(length(object) == 3) {
-    return(combine(object[[1]], object[[2]], object[[3]], by = by, fun = fun)
+    return(combine(object[[1]], object[[2]], object[[3]], by = by, fun = fun))
   }
   if(length(object) == 4) {
-    return(combine(object[[1]], object[[2]], object[[3]], object[[4]], by = by, fun = fun)
+    return(combine(object[[1]], object[[2]], object[[3]], object[[4]], by = by, fun = fun))
   }
   stop("use combine.jags_sample instead with more than 4 jags_samples")
 }
