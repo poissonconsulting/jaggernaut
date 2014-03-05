fee5ab8fa7f83bbf9bdc629fd579a599b7efce6d
Joe Thorley <joe@poissonconsulting.ca>
2014-03-05 15:01:32 -0800

    added fritillary demo and removed non-sacii characters

--- demo/fritillary.r
+++ demo/fritillary.r
@@ -1,57 +1,39 @@
-# Simple Poisson model for fritillary butterfly data set (Kéry & Schaub, 2011, §12.3.1)
-# Patrck M. Hogan, 01 March 2014
+# Simple Poisson model for fritillary butterfly data set (Kery & Schaub, 2011, 12.3.1)
+# Patrick M. Hogan, 01 March 2014
 
-# library(devtools)
-# install_github("jaggernaut", "joethorley", "v1.5.6")    # This demo built on v1.5.6
 library(jaggernaut)
 library(plyr)
 library(reshape2)
-library(pingr)
-rm(list=ls())     # Clear out memory
 
 # We apply an open-population binomial mixture model to estimate population during each day.
 # Count for sites i = 1..95, replicates j = 1,2 and days k = 1..7
-
-data(fritillary)    # Load the fritillary data contained in the jaggernaut package
-fritillary <- rename(fritillary, c("site" = "Site", "day" = "Day", "count1" = "Rep1", "count2" = "Rep2"))
-fritillary.melted <- melt(fritillary, id.vars = c("Site", "Day"), variable.name = "Replicate", value.name = "Count")
-
-# In Kéry & Schaub's code, the data is passed as a 3-dim array. Instead, we melt the data into long format, as preferred in jaggernaut. However, we retain this 3-dim structure in the priors for convinence.
-
-fritillary.melted$Site <- factor(fritillary.melted$Site)
-fritillary.melted$Day <- factor(fritillary.melted$Day)
-fritillary.melted$Replicate <- factor(fritillary.melted$Replicate)
-
-# Specify the model in JAGS using jaggernaut:
-simple.poisson <- jags_model("model{
+model <- jags_model("model{
   for (j in 1:nDay){
-      alpha.lam[j] ~ dnorm(0, 0.01)
-      p[j] ~ dunif(0, 1)
-      
-      lambda[j] <- exp(alpha.lam[j])
-      
-      for(i in 1:nSite){
-          N[i, j] ~ dpois(lambda[j])
-      }
+    alpha.lam[j] ~ dnorm(0, 0.01)
+    p[j] ~ dunif(0, 1)
+    
+    lambda[j] <- exp(alpha.lam[j])
+    
+    for(i in 1:nSite){
+      N[i, j] ~ dpois(lambda[j])
+    }
   }
   
   for (ii in 1:length(Count)){
-      Count[ii] ~ dbin(p[Day[ii]], N[Site[ii], Day[ii]])
-      eval[ii] <- p[Day[ii]] * N[Site[ii], Day[ii]]
-      E[ii] <- ((Count[ii] - eval[ii])^2) / (eval[ii] + 0.5)
-
-      Count.new[ii] ~ dbin(p[Day[ii]], N[Site[ii], Day[ii]])
-      E.new[ii] <- ((Count.new[ii] - eval[ii])^2) / (eval[ii] + 0.5)
+    Count[ii] ~ dbin(p[Day[ii]], N[Site[ii], Day[ii]])
+    eval[ii] <- p[Day[ii]] * N[Site[ii], Day[ii]]
+    E[ii] <- ((Count[ii] - eval[ii])^2) / (eval[ii] + 0.5)
+    
+    Count.new[ii] ~ dbin(p[Day[ii]], N[Site[ii], Day[ii]])
+    E.new[ii] <- ((Count.new[ii] - eval[ii])^2) / (eval[ii] + 0.5)
   }
-
+  
   for(k in 1:nDay){
     totalN[k] <- sum(N[,k])
     mean.abundance[k] <- exp(alpha.lam[k])
   }
-   fit <- sum(E)
-   fit.new <- sum(E.new)
-}",
-derived_code = "model{
+  fit <- sum(E)
+  fit.new <- sum(E.new)
 }",
 gen_inits = function (data) {
     
@@ -69,15 +51,28 @@ gen_inits = function (data) {
     
     return (inits)
 },
-# No random effects
 monitor = c("totalN", "mean.abundance", "alpha.lam", "p", "fit", "fit.new"),
 select = c("Count", "Site", "Day")
 )
 
-opts_jagr(nchains = 3, nsims = 2000) # To accurately parallel the Kéry & Schaub example this should be 3000, but limited by jaggernaut.
-analysis.output <- jags_analysis(simple.poisson, fritillary.melted, niters = 10^4) # Convergence can be elusive, even for niters = 10^5 -- needs fixing!
+# In Kery & Schaub's code, the data is passed as a 3-dim array. Instead, we melt the data into long format, as preferred in jaggernaut. However, we retain this 3-dim structure in the priors for convenience.
+data(fritillary)  
+data <- fritillary
+data <- rename(data, c("site" = "Site", "day" = "Day", "count1" = "Rep1", "count2" = "Rep2"))
+data <- melt(data, id.vars = c("Site", "Day"), variable.name = "Replicate", value.name = "Count")
 
-summary(analysis.output)
+data$Site <- factor(data$Site)
+data$Day <- factor(data$Day)
+data$Replicate <- factor(data$Replicate)
 
-plot(analysis.output$analyses$Model1$chains$samples$fit, analysis.output$analyses$Model1$chains$samples$fit.new, main = "", xlab = "Discrepancy actual data", ylab = "Discrepancy replicate data", frame.plot = FALSE)
-abline(0, 1, lwd = 2, col = "black")
+opts_jagr(nchains = 3, nsims = 3000)
+analysis <- jags_analysis(model, data, niters = 10^4) 
+# Convergence can be elusive, even for niters = 10^5 -- needs fixing!
+
+summary(analysis)
+
+# predict(analysis, parm = "fit")
+# coef(analysis, parm = "fit", level = )
+# 
+# plot(analysis$analyses$Model1$chains$samples$fit, analysis$analyses$Model1$chains$samples$fit.new, main = "", xlab = "Discrepancy actual data", ylab = "Discrepancy replicate data", frame.plot = FALSE)
+# abline(0, 1, lwd = 2, col = "black")
