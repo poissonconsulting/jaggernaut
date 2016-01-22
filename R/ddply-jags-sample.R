2cce051aa637a79abb62cfbf57ce4ae927dc526a
Joe Thorley <joe@poissonconsulting.ca>
2016-01-22 10:55:36 -0800

    Tidy ddply

--- R/ddply-jags-sample.R
+++ R/ddply-jags-sample.R
@@ -8,19 +8,17 @@
 #' @param fun the function to using when combining samples (by default fun = sum). 
 #' @return a jags_sample object
 #' @seealso \code{\link{predict.jags_analysis}}
-#' @importFrom plyr ddply
 #' @export
-ddply_jags_sample <- function (object, by, fun = sum) {
-  if(!is.jags_sample(object))
+ddply_jags_sample <- function(object, by, fun = sum) {
+  if (!is.jags_sample(object))
     stop("object must be class jags_sample")
 
-  fun_vs <- function (x, fun) {
+  fun_vs <- function(x, fun) {
     class(x) <- c("data.frame", "jags_sample")
     samples <- samples(x)
-    return (apply(samples, MARGIN = 2, FUN = fun))
+    return(apply(samples, MARGIN = 2, FUN = fun))
   }
-  
-  object <- ddply(object, .variables = by, fun_vs, fun = fun)
+  object %<>% plyr::ddply(.variables = by, .fun = fun_vs, fun = fun)
   class(object) <- c("data.frame","jags_sample")
-  return (object)
+  return(object)
 }
